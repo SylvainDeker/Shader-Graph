ed2e0a24e47927dfbf40c9172010629ac0a7278d
Sylvain DEKER <sylvain.deker.etd@gmx.com>
2019-04-26 17:29:36 +0200

    Rename mainapp

--- data/shaders/runtime/Material.glsl
+++ data/shaders/runtime/Material.glsl
@@ -35,41 +35,125 @@ struct Material {
     MaterialTextures tex;
 };
 
-/*
- *	Material interface : gives access on the parameter with sementic control
- */
-vec3 getKd(Material material, vec2 texCoord) {
-    if (material.tex.hasKd == 1) {
-        return vec3(texture(material.tex.kd, texCoord));
-    }
+vec3 getKd(Material material, vec2 texCoord) { 
+vec3 Diffuse = vec3(0.0f);  // Input
+vec3 Normal = vec3(0.0f);  // Input
+vec3 Specular = vec3(0.0f);  // Input
+float Metallic = 0.0f;  // Input
+float Roughness = 0.0f;  // Input
+float Emissive = 0.0f;  // Input
+float id9_Result = 0.0f; // Output
+// Input : Scalar 
+id9_Result = 2.300000;
 
-    return material.kd.xyz;
-}
+float id8_Value = id9_Result;  // Input
 
-vec3 getKs(Material material, vec2 texCoord) {
-    if (material.tex.hasKs == 1) {
-        return vec3(texture(material.tex.ks, texCoord));
-    }
+float id8_Step = id9_Result;  // Input
+float id8_Result = 0.0f; // Output
+// Step 
+id8_Result = floor(id8_Value / id8_Step) * id8_Step;
 
-    return material.ks.xyz;
-}
 
-vec2 getRoughness(Material material, vec2 texCoord) {
-    float roug;
+float Opacity = id8_Result;  // Input
+vec3 WorldPositionOffset = vec3(0.0f);  // Input
+vec3 AmbientOcclusion = vec3(0.0f);  // Input
+vec3 Refraction = vec3(0.0f);  // Input
+vec2 TextureCoordinate = vec2(0.0f);  // Input
+vec3 Tangent = vec3(0.0f);  // Input
+float Depth = 0.0f;  // Input
+// End MasterMaterialOutput
+return Diffuse; 
+} 
 
-    if (material.tex.hasRoughness == 1) {
-        roug = texture(material.tex.roughness, texCoord).r;
-    }
-    else {
-        roug =  material.roughness;
-    }
+vec3 getKs(Material material, vec2 texCoord) { 
+vec3 Diffuse = vec3(0.0f);  // Input
+vec3 Normal = vec3(0.0f);  // Input
+vec3 Specular = vec3(0.0f);  // Input
+float Metallic = 0.0f;  // Input
+float Roughness = 0.0f;  // Input
+float Emissive = 0.0f;  // Input
+float id9_Result = 0.0f; // Output
+// Input : Scalar 
+id9_Result = 2.300000;
 
-    if (material.remapRoughness == 1) {
-        roug = TRD_RoughnessToAlpha(roug);
-    }
+float id8_Value = id9_Result;  // Input
 
-    return vec2(roug);
-}
+float id8_Step = id9_Result;  // Input
+float id8_Result = 0.0f; // Output
+// Step 
+id8_Result = floor(id8_Value / id8_Step) * id8_Step;
+
+
+float Opacity = id8_Result;  // Input
+vec3 WorldPositionOffset = vec3(0.0f);  // Input
+vec3 AmbientOcclusion = vec3(0.0f);  // Input
+vec3 Refraction = vec3(0.0f);  // Input
+vec2 TextureCoordinate = vec2(0.0f);  // Input
+vec3 Tangent = vec3(0.0f);  // Input
+float Depth = 0.0f;  // Input
+// End MasterMaterialOutput
+return Specular; 
+} 
+
+vec2 getRoughness(Material material, vec2 texCoord) { 
+vec3 Diffuse = vec3(0.0f);  // Input
+vec3 Normal = vec3(0.0f);  // Input
+vec3 Specular = vec3(0.0f);  // Input
+float Metallic = 0.0f;  // Input
+float Roughness = 0.0f;  // Input
+float Emissive = 0.0f;  // Input
+float id9_Result = 0.0f; // Output
+// Input : Scalar 
+id9_Result = 2.300000;
+
+float id8_Value = id9_Result;  // Input
+
+float id8_Step = id9_Result;  // Input
+float id8_Result = 0.0f; // Output
+// Step 
+id8_Result = floor(id8_Value / id8_Step) * id8_Step;
+
+
+float Opacity = id8_Result;  // Input
+vec3 WorldPositionOffset = vec3(0.0f);  // Input
+vec3 AmbientOcclusion = vec3(0.0f);  // Input
+vec3 Refraction = vec3(0.0f);  // Input
+vec2 TextureCoordinate = vec2(0.0f);  // Input
+vec3 Tangent = vec3(0.0f);  // Input
+float Depth = 0.0f;  // Input
+// End MasterMaterialOutput
+return vec2(Roughness); 
+} 
+
+float getMetalness() { 
+vec3 Diffuse = vec3(0.0f);  // Input
+vec3 Normal = vec3(0.0f);  // Input
+vec3 Specular = vec3(0.0f);  // Input
+float Metallic = 0.0f;  // Input
+float Roughness = 0.0f;  // Input
+float Emissive = 0.0f;  // Input
+float id9_Result = 0.0f; // Output
+// Input : Scalar 
+id9_Result = 2.300000;
+
+float id8_Value = id9_Result;  // Input
+
+float id8_Step = id9_Result;  // Input
+float id8_Result = 0.0f; // Output
+// Step 
+id8_Result = floor(id8_Value / id8_Step) * id8_Step;
+
+
+float Opacity = id8_Result;  // Input
+vec3 WorldPositionOffset = vec3(0.0f);  // Input
+vec3 AmbientOcclusion = vec3(0.0f);  // Input
+vec3 Refraction = vec3(0.0f);  // Input
+vec2 TextureCoordinate = vec2(0.0f);  // Input
+vec3 Tangent = vec3(0.0f);  // Input
+float Depth = 0.0f;  // Input
+// End MasterMaterialOutput
+return Metallic; 
+} 
 
 vec3 getNormal(Material material, vec2 texCoord, vec3 N, vec3 T, vec3 B) {
     if (material.tex.hasNormal == 1) {
@@ -98,29 +182,34 @@ vec3 computeMaterialInternal(Material material, vec2 texC, vec3 wi, vec3 wo) {
      if (cosTi == 0 || cosTo == 0)
          return vec3(0.f);
 
-	
+
      bool reflect = (cosTi * cosTo > 0);
      vec3 result = vec3(0.f);
+
+     vec3 kd = getKd(material, texC);
 
      if (reflect) {
         cosTi = abs(cosTi);
         cosTo = abs(cosTo);
 
-        vec3 kd = getKd(material, texC);
         result = (kd * InvPi);
 
         vec3 wh = wi + wo;
         if (!(wh.x == 0 && wh.y == 0 && wh.z == 0)) {
             wh = normalize(wh);
             vec3 ks = getKs(material, texC);
-            float F = FresnelDielectric(dot(wi, wh), 1, 1.5);
+            vec3 F0 = vec3(0.04);
+            F0 = mix(F0, ks, vec3(getMetalness()));
+            vec3 F90 = kd;
+            vec3 F = FresnelShlick(dot(wi, wh), F0, F90);
             vec2 rough = getRoughness(material, texC);
             float D = TRD_D(wh, rough.x, rough.y);
             float G = TRD_G(wo, wi, rough.x, rough.y);
-            result += (ks * F * D * G /(4 * cosTi * cosTo));
+            result += (F * D * G /(4 * cosTi * cosTo));
         }
      }
-     return result * cosTi;
+     /* result + ambient */
+     return result * cosTi + 0.1 * kd;
 }
 
 // Define the uniform that could be accessed from th application
